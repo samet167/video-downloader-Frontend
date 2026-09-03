fdedc8f664db7794668796c459fc1571e2ca9184
samet168 <samet.moeun9@gmail.com>
2026-09-03 20:44:55 +0700

    fix: update backend URL to Render backend in config.js and index.html

--- js/config.js
+++ js/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
       (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "");
     return isLocal 
       ? "http://127.0.0.1:5000"
-      : "https://compete-newsletter-statistics-encouraging.trycloudflare.com";
+      : "https://video-downloader-backend-1-5oer.onrender.com";
   })(),
 
   /** Milliseconds before an /api/info request is considered timed out */
